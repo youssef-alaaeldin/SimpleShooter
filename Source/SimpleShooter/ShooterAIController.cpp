2fb2a68023bc17c10580f54e7d70f28474b4b127
yousufalaa10 <yousufalaa10@yahoo.com>
2023-08-08 16:03:58 +0300

    AI Movement

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -7,16 +7,24 @@ void AShooterAIController::BeginPlay()
 {
     Super::BeginPlay();
 
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
-    SetFocus(PlayerPawn);
-
-    
 }
 
 void AShooterAIController::Tick(float DeltaSeconds)
 {
+    Super::Tick(DeltaSeconds);
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+   
 
-    MoveToActor(PlayerPawn, 200);
+
+    bool bIsInSight = LineOfSightTo(PlayerPawn); 
+    if (bIsInSight)
+    {
+        MoveToActor(PlayerPawn, 200);
+        SetFocus(PlayerPawn); 
+    }
+    else
+    {
+        ClearFocus(EAIFocusPriority::Gameplay);
+        StopMovement();
+    }
 }
