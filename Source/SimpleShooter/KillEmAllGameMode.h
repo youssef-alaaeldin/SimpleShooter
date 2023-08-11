12beb6d1200b50c564fcd284efa82b6c9768e5d5
yousufalaa10 <yousufalaa10@yahoo.com>
2023-08-11 17:01:47 +0300

    adding lose and win screens

--- Source/SimpleShooter/KillEmAllGameMode.h
+++ Source/SimpleShooter/KillEmAllGameMode.h
@@ -18,5 +18,9 @@ class SIMPLESHOOTER_API AKillEmAllGameMode : public ASimpleShooterGameModeBase
 public:
 
 	virtual void PawnKilled(APawn* PawnKilled) override;
+
+private:
+	
+	void EndGame(bool bIsPlayerWinner);
 	
 };
--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -3,11 +3,34 @@
 
 #include "ShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void AShooterPlayerController::GameHasEnded(AActor *EndGameFocus, bool bIsWinner)
 {
 
     Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+    if (bIsWinner)
+    {
+        UUserWidget* WinScreen = CreateWidget(this, WinScreenClass);  
+
+        if(WinScreen != nullptr)
+        {
+            WinScreen->AddToViewport();
+        }
+    }
+
+    else
+    {
+        UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);  
+
+    if(LoseScreen != nullptr)
+    {
+        LoseScreen->AddToViewport();
+    }
+    }
+    
+    
 
     GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
